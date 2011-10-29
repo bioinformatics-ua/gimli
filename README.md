521de9c8a7c7a07d4e532d1ba1f21a2c2b02fb26
David Campos <david.campos@ua.pt>
2011-10-29 04:17:42 +0100

    Final modifications for the first release of Gimli.

---
+++ README.md
@@ -0,0 +1,45 @@
+GIMLI
+=====
+Gimli is a library and tool for high-performance and multi-corpus recognition of biomedical entity names.
+
+Go to http://bioinformatics.ua.pt/gimli for more information.
+
+Documentation
+-------------
+Full Documentation: http://bioinformatics.ua.pt/gimli/doc
+	   API Javadoc:	http://bioinformatics.ua.pt/gimli/apidoc
+
+Bug tracker
+-----------
+Have a bug? Please create an issue here on GitHub!
+
+https://github.com/davidcampos/gimli/issues
+
+Project Structure
+-----------------
+This project is organized as follows:
+config/							- model configuration files
+resources/
+	corpus/						- used corpora
+	evaluation/					- evaluation scripts
+	models/						- trained models
+	tools/						- internal tools
+gimli.sh						- executable
+src/main/
+	java/						- source code
+	resources/
+		lexicons/				- biomedical lexicons
+		models/					- trained models for internal tools
+
+Team
+----
+	  David Campos: david.campos(a)ua.pt 	[http://davidcampos.pt.vu]
+	  Sérgio Matos: aleixomatos(a)ua.pt
+José Luís Oliveira: jlo(a)ua.pt 			[http://www.ieeta.pt/~jlo]
+
+
+License
+-------
+Copyright (C) 2011 David Campos, Universidade de Aveiro, Instituto de Engenharia Electrónica e Telemática de Aveiro
+
+Gimli is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License. To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/.
--- src/main/java/pt/ua/tm/gimli/util/Model.java
+++ src/main/java/pt/ua/tm/gimli/util/Model.java
@@ -1,23 +1,20 @@
 /*
- *  Gimli - High-performance and multi-corpus recognition of biomedical
- *  entity names
+ * Gimli - High-performance and multi-corpus recognition of biomedical
+ * entity names
  *
- *  Copyright (C) 2011 David Campos, Universidade de Aveiro, Instituto de
- *  Engenharia Electrónica e Telemática de Aveiro
+ * Copyright (C) 2011 David Campos, Universidade de Aveiro, Instituto de
+ * Engenharia Electrónica e Telemática de Aveiro
  *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version. Thus, this program could not be
- *  used for commercial purposes.
+ * Gimli is licensed under the Creative Commons
+ * Attribution-NonCommercial-ShareAlike 3.0 Unported License. To view a copy of
+ * this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ * Gimli is a free software, you are free to copy, distribute, change and
+ * transmit it. However, you may not use Gimli for commercial purposes.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * Gimli is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.
  *
  */
 package pt.ua.tm.gimli.util;
