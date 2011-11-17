e74108609047571881993e58f444971cf6214f56
David Campos <david@new-host.home>
2011-11-17 18:49:17 +0000

    Set sentence identifier on clone.

--- src/main/java/pt/ua/tm/gimli/corpus/Sentence.java
+++ src/main/java/pt/ua/tm/gimli/corpus/Sentence.java
@@ -474,6 +474,8 @@ public class Sentence {
         for (int i = 0; i < annotations.size(); i++) {
             s.addAnnotation(annotations.get(i).clone(s));
         }
+        
+        s.setId(getId());
 
         return s;
     }
